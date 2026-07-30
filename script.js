993898c481376b3666570056bccb516a9c5b4211
Quackquack01 <rongaming72720@gmail.com>
2026-07-30 19:14:55 +0530

    feat: Add back to top button

--- script.js
+++ script.js
@@ -237,3 +237,22 @@ document.querySelectorAll("button,.heroBtn,.featuredBtn").forEach(btn=>{
         setTimeout(()=>ripple.remove(),600);
     });
 });
+
+/* ===== Back To Top ===== */
+
+const backToTop=document.getElementById("backToTop");
+
+window.addEventListener("scroll",()=>{
+    if(window.scrollY>400){
+        backToTop.classList.add("show");
+    }else{
+        backToTop.classList.remove("show");
+    }
+});
+
+backToTop.addEventListener("click",()=>{
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+});
